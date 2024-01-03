0f73435727d5398ccf24680671d9c72600ec43b5
Dominic Chow <ch888ow@gmail.com>
2024-01-03 23:00:44 +0800

    Update submitProposal.js

--- pages/api/submitProposal.js
+++ pages/api/submitProposal.js
@@ -7,6 +7,9 @@ export default async function handler(req, res) {
     if (!proposal) {
       return res.status(200).json({ message: "Please fill in all fields" });
     }
+
+    proposal.status = "pending";
+
     insertData(proposal, "Proposals").then((result) => {
       res.status(200).json({ message: "Proposal submitted" });
     });
