f90ce90fa47942e1cabd15e92ca327fe21632844
Dominic cwc <80381944+Dominic-cwc@users.noreply.github.com>
2024-03-08 19:05:44 +0800

    Update input.jsx

--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
     <input
       type={type}
       className={cn(
-        // when used flex, it become a flex container, which is independent, so pushed down which opens a newline
+        // when used flex, it become a flex container, which is a block level element, so pushed down which opens a newline
         "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
