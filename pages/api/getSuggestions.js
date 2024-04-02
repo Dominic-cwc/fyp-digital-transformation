9a7a14a009e07253c1bb0891d525d5268d04118d
Dominic cwc <80381944+Dominic-cwc@users.noreply.github.com>
2024-04-02 16:11:50 +0800

    Create getSuggestions.js

---
+++ pages/api/getSuggestions.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export default async (req, res) => {
+  return new Promise((resolve) => {
+    if (req.method === "POST") {
+      const proposalData = req.body;
+
+      axios
+        .post(
+          `${process.env.PYTHON_BACKEND_URL}/api/getSuggestions`,
+          proposalData
+        )
+        .then((response) => {
+          res.status(200).json(response.data);
+          resolve();
+        })
+        .catch((error) => {
+          res.status(500).json({ message: error.message });
+          resolve();
+        });
+    } else if (req.method === "OPTIONS") {
+      res.status(200).json({ acceptedMethods: "POST" });
+    } else {
+      res.status(405).json({ message: "Method not allowed" });
+    }
+  });
+};
